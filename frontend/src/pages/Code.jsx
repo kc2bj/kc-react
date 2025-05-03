54de245c1f372dfc5c0ae9ae19739ec8097f1805
Kris Celeste <49953388+kc2bj@users.noreply.github.com>
2025-05-03 10:27:08 -0400

    github contributions

--- frontend/src/pages/Code.jsx
+++ frontend/src/pages/Code.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import "../styles/global.css";
 
 export default function Code() {
   const [contributions, setContributions] = useState([]);
